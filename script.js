74cab99b39c5e7260f0ef1a4de292551056b8f6e
Lincoln-Smith <Lincolnsmith628@gmail.com>
2024-02-14 13:48:01 -0500

    selecting buttons done!

---
+++ script.js
@@ -0,0 +1,18 @@
+
+ // selects rock 
+function selectRock() {
+    document.getElementById("info").innerText = "You selected ROCK"
+    document.getElementById("rock").src = "imgs/rock-select.png";
+ }
+ 
+ // selects paper 
+ function selectPaper() {
+     document.getElementById("info").innerText = "You selected PAPER"
+     document.getElementById("paper").src = "imgs/paper-select.png";
+ }
+ 
+ // selects scissors 
+ function selectScissors() {
+     document.getElementById("info").innerText =  "You selected SCISSORS"
+     document.getElementById("scissors").src = "imgs/scissors-select.png";
+ }
