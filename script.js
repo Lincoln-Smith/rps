d08e0777bdbd8eda6c3cfaa588ce0c8a8cf7c6a6
Lincoln-Smith <Lincolnsmith628@gmail.com>
2024-02-15 14:44:01 -0500

    squeeop

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 
  // selects rock 
-function selectRock() {
+ function selectRock() {
     document.getElementById("info").innerText = "You selected ROCK"
     document.getElementById("rock").src = "imgs/rock-select.png";
+    document.getElementById("paper").src = "imgs/paper-neutral.png";
+    document.getElementById("scissors").src = "imgs/scissors-neutral.png";
  }
  
  // selects paper 
  function selectPaper() {
      document.getElementById("info").innerText = "You selected PAPER"
      document.getElementById("paper").src = "imgs/paper-select.png";
+     document.getElementById("rock").src = "imgs/rock-neutral.png";
+     document.getElementById("scissors").src = "imgs/scissors-neutral.png";
  }
  
  // selects scissors 
  function selectScissors() {
      document.getElementById("info").innerText =  "You selected SCISSORS"
      document.getElementById("scissors").src = "imgs/scissors-select.png";
+     document.getElementById("paper").src = "imgs/paper-neutral.png";
+     document.getElementById("rock").src = "imgs/rock-neutral.png";
  }
